ff2faa6d79dee1a7cfe8c0f7b7f7e4d62e89403e
stackoverflow911 <31153429+stackoverflow911@users.noreply.github.com>
2017-08-24 19:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var page1 = {
             -Dont worry about turning on/off your light we do it for u. <hr>
             2.Automatic Cooling system <br>
             -Dont worry about turning on your ac or fan we are always happy to help. <hr>
-            <hr><hr>
+            <br><br>
             More features coming soon.
             
             
