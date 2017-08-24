5f27c8d98376478335885783154b5dddaafec22a
stackoverflow911 <31153429+stackoverflow911@users.noreply.github.com>
2017-08-24 14:28:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ghanta', function (req, res) {
+  res.send("Tera babaji ka ghanta");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
