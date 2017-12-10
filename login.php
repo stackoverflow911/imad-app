01120ad391f240232b70b78f9caebb74351f5cc7
stackoverflow911 <31153429+stackoverflow911@users.noreply.github.com>
2017-12-10 14:41:18 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
-$connection = mysql_connect("localhost", "root", ""); // Establishing connection with server..
+$connection = mysql_connect("localhost:5432", "root", ""); // Establishing connection with server..
 $db = mysql_select_db("college", $connection); // Selecting Database.
-$email=$_POST['email1']; // Fetching Values from URL.
-$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
+$email=$_POST['rumanmulla911@ssh.imad.hasura-app.io']; // Fetching Values from URL.
+$password= sha1($_POST['db-rumanmulla911-46442']); // Password Encryption, If you like you can also leave sha1.
 // check if e-mail address syntax is valid or not
 $email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
