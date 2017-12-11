3adf233869daf2be9f829ec1cf9a52c79edd6a9e
stackoverflow911 <31153429+stackoverflow911@users.noreply.github.com>
2017-12-11 13:44:31 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,20 +1,36 @@
-$connection = mysql_connect("localhost:5432", "rumanmulla911", "db-rumanmulla911-46442"); // Establishing connection with server..
-$db = mysql_select_db("college", $connection); // Selecting Database.
-$email=$_POST['rumanmulla911@ssh.imad.hasura-app.io']; // Fetching Values from URL.
-$password= sha1($_POST['db-rumanmulla911-46442']); // Password Encryption, If you like you can also leave sha1.
-// check if e-mail address syntax is valid or not
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Invalid Email.......";
-}else{
-// Matching user input email and password with stored email and password in database.
-$result = mysql_query("SELECT * FROM registration WHERE email='$email' AND password='$password'");
-$data = mysql_num_rows($result);
-if($data==1){
-echo "Successfully Logged in...";
-}else{
-echo "Email or Password is wrong...!!!!";
-}
-}
-mysql_close ($connection); // Connection Closed.
-?>
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
